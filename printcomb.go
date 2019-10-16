1a85ab6d5ae50980960b35b3cadacab4d202e97f
serzhansafin <safin_serzhan@mail.ru>
2019-10-16 23:12:45 +0600

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -6,23 +6,23 @@ func PrintComb() {
 
 	for i := '0'; i <= '7'; i++ {
 		for j := '1'; j <= '8'; j++ {
-			for k := '2'; k<= '9'; k++{
-				
+			for k := '2'; k <= '9'; k++ {
+
 				if i < j && j < k {
 
-                	z01.PrintRune(i)
+					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
 
-				if i != '7' || j != '8' || k != '9' {
-				
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+					if i != '7' || j != '8' || k != '9' {
+
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 
 					}
 				}
 			}
 		}
 	}
-z01.PrintRune('\n')
-}	
+	z01.PrintRune('\n')
+}
