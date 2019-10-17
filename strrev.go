275dba4d90f7fb38597b5d0f3e1794b73eb20126
serzhansafin <safin_serzhan@mail.ru>
2019-10-17 22:09:00 +0600

    strrev

--- strrev.go
+++ strrev.go
@@ -1,9 +1,20 @@
 package piscine
 
+func StrLep(str string) int {
+
+	sentence := []rune(str)
+	var num int
+	for index := range sentence {
+
+		num = index
+	}
+	return num + 1
+}
+
 func StrRev(s string) string {
 
 	reverse := []rune(s)
-	for i, j := 0, len(reverse)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, StrLep(s)-1; i < j; i, j = i+1, j-1 {
 		reverse[i], reverse[j] = reverse[j], reverse[i]
 	}
 	return string(reverse)
