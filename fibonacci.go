4458139fa360e867b5a9dac7a0e9817db875e67a
serzhansafin <safin_serzhan@mail.ru>
2019-10-21 16:14:20 +0600

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -10,7 +10,7 @@ func Fibonacci(index int) int {
 	if index == 1 || index == 2 {
 		return 1
 	}
-	if index > 2 || index < 100 {
+	if index > 2 || index < 20 {
 		return Fibonacci(index-2) + Fibonacci(index-1)
 	}
 	return 0
