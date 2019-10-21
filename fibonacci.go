81438d9cb95fc73696272f1ed35a719546373e13
serzhansafin <safin_serzhan@mail.ru>
2019-10-21 16:38:53 +0600

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -1,24 +1,21 @@
-package piscine
+package main
 
-/*
 import (
 	"fmt"
 )
-*/
 
 func Fibonacci(index int) int {
 	if index == 1 || index == 2 {
 		return 1
 	}
-	if index > 2 || index < 15 {
+	if index > 2 && index < 15 {
 		return Fibonacci(index-2) + Fibonacci(index-1)
 	}
+
 	return 0
 }
 
-/*
 func main() {
-	arg1 := 20
+	arg1 := 0
 	fmt.Println(Fibonacci(arg1))
 }
-*/
