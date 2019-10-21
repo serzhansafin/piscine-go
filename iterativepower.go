34db4e3f94c682be135b975f374741debc2f9e91
serzhansafin <safin_serzhan@mail.ru>
2019-10-21 14:44:21 +0600

    iterativepower

---
+++ iterativepower.go
@@ -0,0 +1,29 @@
+package piscine
+
+/*
+import (
+	"fmt"
+)
+*/
+
+func IterativePower(nb int, power int) int {
+
+	if nb < 18 && nb >= 0 && power >= 0 {
+		result := 1
+		for i := 1; i <= power; i++ {
+			result *= nb
+		}
+		return result
+	}
+
+	return 0
+
+}
+
+/*
+func main() {
+	arg1 := 1
+	arg2 := 3
+	fmt.Println(IterativePower(arg1, arg2))
+}
+*/
