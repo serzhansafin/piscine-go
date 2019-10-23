54f9ea62c7f82b79663c3ea1eca82a53a8b0677b
serzhansafin <safin_serzhan@mail.ru>
2019-10-23 21:00:00 +0600

    printparams

---
+++ printparams/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/01-edu/z01"
+import (
+	"os"
+)
+
+func main() {
+	params := os.Args
+	length := 0
+
+	for index := range params {
+		length = index
+	}
+
+	for i := 1; i <= length; i++ {
+		for _, w := range params[i] {
+			z01.PrintRune(w)
+
+		}
+		z01.PrintRune('\n')
+	}
+}
