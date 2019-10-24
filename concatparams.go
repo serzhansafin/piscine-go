ed32310d92130f1bf5efc87f631da6701e84d3d6
serzhansafin <safin_serzhan@mail.ru>
2019-10-24 19:28:35 +0600

    concatparams

---
+++ concatparams.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func ConcatParams(args []string) string {
+
+	var index int
+	var res string
+
+	for i := range args {
+
+		index = i
+	}
+
+	for i := 0; i <= index; i++ {
+		if i != index {
+			res = res + args[i] + "\n"
+		} else {
+			res = res + args[i]
+
+		}
+
+	}
+	return res
+}
+
+func main() {
+	test := []string{"Hello", "how", "are", "you?"}
+	fmt.Println(ConcatParams(test))
+}
